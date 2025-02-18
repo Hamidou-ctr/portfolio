faad35464c7056486da83afbbc708215d909c696
Hamidou <hamiduguinea@gmail.com>
2025-02-18 21:56:31 +0100

    Refactor project component; implement dynamic project rendering, enhance layout and styles for improved user experience and visual consistency

--- src/app/portfolio/project/project.component.ts
+++ src/app/portfolio/project/project.component.ts
@@ -10,4 +10,38 @@ import { Component } from '@angular/core';
 })
 export class ProjectComponent {
 
+  projects = [
+    {
+      imageUrl: './../../../assets/img/Pollo loco 1.png',
+      name: 'Pollo Loco Game',
+      language: 'JavaScript | HTML | CSS',
+      description: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+      liveTestUrl: 'https://diallo-hamidou.developerakademie.net/pollo-loco-game/#game',
+      githubUrl: 'https://github.com/Hamidou-ctr/pollo-loco',
+    },
+    {
+      imageUrl: './../../../assets/img/left.png',
+      name: 'Projekt 2',
+      language: 'Angular',
+      description: 'Ein Projekt mit Angular erstellt.',
+      liveTestUrl: 'https://example.com/project2',
+      githubUrl: 'https://github.com/Hamidou-ctr/project2',
+    },
+    {
+      imageUrl: './../../../assets/img/purple_reference.png',
+      name: 'Projekt 3',
+      language: 'React',
+      description: 'Ein Projekt mit React erstellt.',
+      liveTestUrl: 'https://example.com/project3',
+      githubUrl: 'https://github.com/Hamidou-ctr/project3',
+    },
+    {
+      imageUrl: './../../../assets/img/profil.png',
+      name: 'Projekt 4',
+      language: 'Vue.js',
+      description: 'Ein Projekt mit Vue.js erstellt.',
+      liveTestUrl: 'https://example.com/project4',
+      githubUrl: 'https://github.com/Hamidou-ctr/project4',
+    },
+  ];
 }
