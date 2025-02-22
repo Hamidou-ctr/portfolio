43bbf011445724bb1dd2fb4f8788b2ee01d39ed4
Hamidou <hamiduguinea@gmail.com>
2025-02-22 09:04:54 +0100

    Refactor components for improved navigation and layout; add IDs for smooth scrolling, enhance button structure, and adjust styles for better alignment and responsiveness

--- src/app/portfolio/project/project.component.ts
+++ src/app/portfolio/project/project.component.ts
@@ -24,24 +24,17 @@ export class ProjectComponent {
       name: 'Join',
       language: 'JavaScript | HTML | CSS | Firebase ',
       description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
-      liveTestUrl: 'https://example.com/project2',
-      githubUrl: 'https://github.com/Hamidou-ctr/project2',
+      liveTestUrl: 'https://hamidou-diallo.com/angular-projects/ring-of-fire',
+      githubUrl: 'https://github.com/Hamidou-ctr',
     },
-    {
+  /*   {
       imageUrl: './../../../assets/img/purple_reference.png',
       name: 'Projekt 3',
       language: 'React',
       description: 'Ein Projekt mit React erstellt.',
-      liveTestUrl: 'https://example.com/project3',
+      liveTestUrl: 'https://hamidou-diallo.com/angular-projects/ring-of-fire',
       githubUrl: 'https://github.com/Hamidou-ctr/project3',
     },
-    {
-      imageUrl: './../../../assets/img/profil.png',
-      name: 'Projekt 4',
-      language: 'Vue.js',
-      description: 'Ein Projekt mit Vue.js erstellt.',
-      liveTestUrl: 'https://example.com/project4',
-      githubUrl: 'https://github.com/Hamidou-ctr/project4',
-    },
+    */
   ];
 }
