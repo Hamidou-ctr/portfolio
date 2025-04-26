e0d66e5c7f24caed02c2170af9393eab72bcb600
Hamidou <hamiduguinea@gmail.com>
2025-04-26 23:42:18 +0200

    Refactor About Me component styles; enhance layout and responsiveness, add smooth transitions, and remove unused code

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -12,3 +12,47 @@ import { TranslateModule } from '@ngx-translate/core';
 export class AboutMeComponent {
 
 }
+
+
+
+
+
+/* 
+
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, OnDestroy, AfterViewInit } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-about-me',
+  standalone: true,
+  imports: [CommonModule, TranslateModule],
+  templateUrl: './about-me.component.html',
+  styleUrl: './about-me.component.scss'
+})
+export class AboutMeComponent implements AfterViewInit, OnDestroy {
+  isLargeScreen: boolean = false;
+  private resizeObserver!: ResizeObserver;
+
+  constructor(private elRef: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    this.resizeObserver = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        const width = entry.contentRect.width;
+        this.isLargeScreen = width > 1440;
+      }
+    });
+
+    this.resizeObserver.observe(this.elRef.nativeElement.ownerDocument.body);
+  }
+
+  ngOnDestroy(): void {
+    if (this.resizeObserver) {
+      this.resizeObserver.disconnect();
+    }
+  }
+}
+
+
+*/
