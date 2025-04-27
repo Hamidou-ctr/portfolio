6031dcb2a544062bf6c1fd5b57190cb4d21b03c2
Hamidou <hamiduguinea@gmail.com>
2025-04-27 23:16:24 +0200

    Add translation support to Legal Notice and Privacy Policy components

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
