f1028b0391b360b2a7f25f6cd0fd21dc9fc1f7c1
Hamidou <hamiduguinea@gmail.com>
2025-04-27 13:20:21 +0200

    Refactor Contact Me component; add References component, adjust styles for improved layout and responsiveness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,14 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { MySkillsComponent } from "./my-skills/my-skills.component";
 import { PortfolioComponent } from "./portfolio/portfolio.component";
-
+import { ReferencesComponent } from "./references/references.component";
 import { ContactMeComponent } from "./contact-me/contact-me.component";
 import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, HomePageComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent,  ContactMeComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, HomePageComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, ReferencesComponent, ContactMeComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
