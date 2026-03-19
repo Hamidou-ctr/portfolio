a99572c224fbe3973917a45535936a5df937f677
Hamidou-ctr <hamiduguinea@gmail.com>
2026-03-19 16:54:01 +0100

    fix: standardize HTML doctype and format TypeScript error handling

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,13 +1,13 @@
 declare module 'aos' {
-    const aos: {
-        init(config?: {
-            duration?: number;
-            easing?: string;
-            once?: boolean;
-            mirror?: boolean;
-        }): void;
-        refresh(): void;
-        //refreshHard(): void;
-    };
-    export = aos;
+  const aos: {
+    init(config?: {
+      duration?: number;
+      easing?: string;
+      once?: boolean;
+      mirror?: boolean;
+    }): void;
+    refresh(): void;
+    //refreshHard(): void;
+  };
+  export = aos;
 }
